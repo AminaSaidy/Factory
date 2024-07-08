f3a67ddfe32d7c7da09ea3d5f48fbbc3c64a2e1d
Amina Saidaxmedova <aminasaidakhmedova@gmail.com>
2024-07-08 22:35:36 +0500

    made a Singleton pattern for DogFactory

--- src/main/java/uzb/aminasaidakhmedova/factory/entity/DogFactory.java
+++ src/main/java/uzb/aminasaidakhmedova/factory/entity/DogFactory.java
@@ -1,4 +1,15 @@
 package uzb.aminasaidakhmedova.factory.entity;
 
 public class DogFactory {
+    private static DogFactory instance;
+
+    private DogFactory() {
+    }
+
+    public static DogFactory getInstance() {
+        if (instance == null) {
+            instance = new DogFactory();
+        }
+        return instance;
+    }
 }
